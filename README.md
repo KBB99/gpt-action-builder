7b7156239ddb82977fd7c046bb28aabb5c4407cd
Kenton Blacutt <kenton@blacutt.com>
2023-07-05 23:24:59 -0400

    Starting map

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ pip install openai
 
 To see the gpt-action-builder in action put your email in main.py instead of <recipient's-email> and run:
 ```bash
-python main.py
+python main.py --goal "Your goal"
 ```
--- main.py
+++ main.py
@@ -250,7 +250,8 @@ def main(goal, max_meta_iters=1):
         constraints = "You cannot use the open command. Everything must be done in the terminal."
         tips = 'You are in a mac zshell.'\
         'To write to a file, use the echo command.'\
-        'If you are going to run a comand that stays running, run it in the background.'\
+        'You start in an empty playground directory.'\
+        'If you are going to run a blocking operation, make sure to ALWAYS run it in the background.'\
         f"Relevant Actions:{relevant_previous_actions}"
         evaluation_prompt_template = """
         {execution_output}
